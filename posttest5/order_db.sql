cee40308444f54558947fd219dc9af99acb354ea
Aspa <hafizalasfawadhiah4@gmail.com>
2024-10-09 22:11:53 +0800

    Add files via upload

---
+++ posttest5/order_db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE order_db;
+USE order_db;
+
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    menu_item VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    note TEXT
+);
